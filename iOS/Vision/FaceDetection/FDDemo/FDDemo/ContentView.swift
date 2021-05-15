25e156f9c2de9f783edcd00a981a262516e6fdd4
Rohan Bhale <rohanbhale@Rohans-MacBook-Pro.local>
2021-05-15 21:13:48 +0530

    Created the project FDDemo for iOS ML portfolio

---
+++ iOS/Vision/FaceDetection/FDDemo/FDDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FDDemo
+//
+//  Created by Rohan Bhale on 15/05/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOS/Vision/FaceDetection/FDDemo/FDDemo/FDDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FDDemoApp.swift
+//  FDDemo
+//
+//  Created by Rohan Bhale on 15/05/21.
+//
+
+import SwiftUI
+
+@main
+struct FDDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
