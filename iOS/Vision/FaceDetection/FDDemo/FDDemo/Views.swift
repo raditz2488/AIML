b5d31e62097f61ba66943bbe6615e1086420d69c
Rohan Bhale <rohanbhale@Rohans-MacBook-Pro.local>
2021-05-16 13:04:18 +0530

    Project: FDDemo
    Update:
    1. Added code to show image picker or camera control.
    2. Added code to display ui for the app.
    3. Added button to initiate detection of faces.

--- iOS/Vision/FaceDetection/FDDemo/FDDemo/Views.swift
+++ iOS/Vision/FaceDetection/FDDemo/FDDemo/Views.swift
@@ -37,7 +37,7 @@ struct TwoStateButton: View {
     }
 }
 
-struct Main: View {
+struct MainView: View {
     private let image: UIImage
     private let text: String
     private let button: TwoStateButton
@@ -60,5 +60,85 @@ struct Main: View {
         self.image = image
         self.text = text
         self.button = button()
+    }
+}
+
+struct ImagePicker: UIViewControllerRepresentable {
+    typealias UIViewControllerType = UIImagePickerController
+    
+    private(set) var selectedImage: UIImage?
+    private(set) var cameraSource: Bool
+    private let completion: (UIImage?) -> ()
+    
+    init(camera: Bool = false, completion: @escaping(UIImage?) -> ()) {
+        self.cameraSource = camera
+        self.completion = completion
+    }
+    
+    func makeCoordinator() -> ImagePicker.Coordinator {
+        let coordinator = Coordinator(self)
+        coordinator.completion = self.completion
+        return coordinator
+    }
+    
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = context.coordinator
+        imagePickerController.sourceType = cameraSource ? .camera : .photoLibrary
+        
+        return imagePickerController
+    }
+    
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {
+        
+    }
+    
+    class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+        var parent: ImagePicker
+        var completion: ((UIImage?) -> ())?
+        
+        init(_ imagePickerControllerWrapper: ImagePicker) {
+            self.parent = imagePickerControllerWrapper
+        }
+        
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            print("Image picker complete...")
+            
+            let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
+            
+            picker.dismiss(animated: true)
+            
+            completion?(selectedImage)
+        }
+        
+        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+            print("Image picker cancelled...")
+            picker.dismiss(animated: true)
+            completion?(nil)
+        }
+    }
+}
+
+extension UIImage {
+    func fixOrientation() -> UIImage? {
+        UIGraphicsBeginImageContext(self.size)
+        self.draw(at: .zero)
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return newImage
+    }
+    
+    var cgImageOrientation: CGImagePropertyOrientation {
+        switch self.imageOrientation {
+        case .up: return .up
+        case .down: return .down
+        case .left: return .left
+        case .right: return .right
+        case .upMirrored: return .upMirrored
+        case .downMirrored: return .down
+        case .leftMirrored: return .leftMirrored
+        case .rightMirrored: return .rightMirrored
+        }
     }
 }
