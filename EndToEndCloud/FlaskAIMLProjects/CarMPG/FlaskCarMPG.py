b730352eed34f6fa735fc01ea91054606c14d20a
Rohan Bhale <rohanbhale@Rohans-MacBook-Pro.local>
2021-05-21 19:56:11 +0530

    Project: CarMPG Update: Added a map for boundries of features. Also the model is loaded from disk. This all is done in FlaskCarMPG.py.

--- EndToEndCloud/FlaskAIMLProjects/CarMPG/FlaskCarMPG.py
+++ EndToEndCloud/FlaskAIMLProjects/CarMPG/FlaskCarMPG.py
@@ -6,3 +6,15 @@ import uuid
 
 app = Flask(__name__)
 
+Expected = {'cylinders': {'min': 3, 'max': 8},
+ 'displacement': {'min': 68.0, 'max': 455.0},
+ 'horsepower': {'min': 46.0, 'max': 230.0},
+ 'weight': {'min': 1613, 'max': 5140},
+ 'acceleration': {'min': 8.0, 'max': 24.8},
+ 'year': {'min': 70, 'max': 82},
+ 'origin': {'min': 1, 'max': 3}
+ }
+
+ model = load_model('./mpg_model.h5')
+
+ 
